100301df370ea1fb764876a20cfb497f48c3bb2c
Albert Wohletz <albert@wohletz.net>
2014-07-23 20:22:09 -0700

    added basic db to store chars.

--- Display/views.py
+++ Display/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from API import models
 
 # Create your views here.
 def initiative(request):
-    context = {}
+    chars = models.Chars.objects.all()
+    context = {'chars': chars}
     return render(request, 'initiative.html', context)
